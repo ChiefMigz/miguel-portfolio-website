b9f0659371c89ea8be14b399031660b3263d53d1
ChiefMigz <pebenitomiguel@gmail.com>
2022-05-16 14:53:36 -0700

    Fixed mobile display overlap

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,9 +14,9 @@ import Figma from '../assets/figma.jpg';
 
 const Skills = () => {
     return ( 
-        <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        <div name='skills' className='w-full h-full bg-[#0a192f] text-gray-300'>
             {/* Container */}
-            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full mt-8'>
                 <div>
                     <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
                     <p className='py-4'>These are the technologies I've worked with</p>
