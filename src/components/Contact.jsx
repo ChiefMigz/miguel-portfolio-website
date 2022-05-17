f8fbfbda3f6d9330d9e1d5fb792638977925d707
ChiefMigz <pebenitomiguel@gmail.com>
2022-05-17 00:46:54 -0700

    Adjusted mobile display

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-full bg-[#0a192f] flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/1b46e6b0-e942-41f5-bc50-842c817f2021" className='flex flex-col max-w-[600px] w-full mt-8'>
+        <form method='POST' action="https://getform.io/f/1b46e6b0-e942-41f5-bc50-842c817f2021" className='flex flex-col max-w-[600px] w-full mt-24'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - <a className='underline' href="mailto:pebenitomiguel@gmail.com">
