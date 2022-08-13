80a1039e509eeaec28438097f2db95cb72084ac5
ChiefMigz <pebenitomiguel@gmail.com>
2022-08-13 16:44:15 -0700

    Added work section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { Link } from 'react-scroll';
 import { FaBars, FaTimes, FaGithub, FaFacebook ,FaLinkedin } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/logo.png';
 
 const Navbar = () => {
@@ -32,6 +31,10 @@ const Navbar = () => {
             <li className='hover:underline'>
                 <Link to='skills' smooth={true} duration={500}>Skills</Link>
             </li>
+
+            <li className='hover:underline'>
+                <Link to='work' smooth={true} duration={500}>Work</Link>
+            </li>
             
             <li className='hover:underline'>
                 <Link to='contact' smooth={true} duration={500}>Contact</Link>
@@ -59,6 +62,9 @@ const Navbar = () => {
                 <Link onClick={() => handleClick()} to='skills' smooth={true} duration={500}>Skills</Link>
             </li>
             <li className='py-6 text-4xl'>
+                <Link onClick={() => handleClick()} to='work' smooth={true} duration={500}>Work</Link>
+            </li>
+            <li className='py-6 text-4xl'>
                 <Link onClick={() => handleClick()} to='contact' smooth={true} duration={500}>Contact</Link>
             </li>
         </ul>
@@ -67,46 +73,32 @@ const Navbar = () => {
         <div className='hidden sm:flex list-none fixed flex-col top-[35%] left-0'>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500'>
                 <a className='flex justify-between items-center w-full text-gray-300'
-                 href='https://www.facebook.com/ChiefMigz08/'>
+                 href='https://www.facebook.com/ChiefMigz08/' target='_blank'>
                     Facebook <FaFacebook size={30}/>
                 </a>
             </li>
 
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-700'>
                 <a className='flex justify-between items-center w-full text-gray-300'
-                 href='https://www.linkedin.com/in/miguel-shelwyn-pe-benito-052b401b1/'>
+                 href='https://www.linkedin.com/in/miguel-shelwyn-pe-benito-052b401b1/' target='_blank'>
                     LinkedIn <FaLinkedin size={30}/>
                 </a>
             </li>
 
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
                 <a className='flex justify-between items-center w-full text-gray-300'
-                 href='https://github.com/ChiefMigz'>
+                 href='https://github.com/ChiefMigz' target='_blank'>
                     Github <FaGithub size={30}/>
                 </a>
             </li>
 
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
                 <a className='flex justify-between items-center w-full text-gray-300'
-                 href='mailto:pebenitomiguel@gmail.com'>
+                 href='mailto:pebenitomiguel@gmail.com' target='_blank'>
                     Mail <HiOutlineMail size={30}/>
                 </a>
             </li>
-
-            {
-            /*
-            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                <a className='flex justify-between items-center w-full text-gray-300'
-                 href='/'>
-                    Resume < BsFillPersonLinesFill size={30}/>
-                </a>
-            </li>
-            */
-            }
-
         </div>
-
-       
     </div>
     );
 }
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,20 +1,33 @@
-import WorkImg from '../assets/workImg.jpeg';
+import {AiFillCloseCircle} from 'react-icons/ai';
+import { useState } from 'react';
 
 const Work = () => {
+    const [demo, toggleDemo] = useState(false);
+    const handleClick = () => toggleDemo(!demo);
+
+    const WebDesignImg = require('../assets/web-design-img.png')
+    const PongGameImg = require('../assets/pong-img.png')
+    const ThisWebsiteImg = require('../assets/this-website-img.jpeg')
     return ( 
         <div name='work' className='w-full h-full bg-[#0a192f] text-gray-300 my-[100px]'>
+            {/* Game Demo */}
+            <div className={!demo ? 'hidden' : 'z-10 absolute w-[95%] left-10 bg-transparent'}>
+                <AiFillCloseCircle className='m-2 ml-[91vw] hover:fill-rose-700' size={40} onClick={() => handleClick()}/>
+                    <div>
+                        <iframe frameborder="0" width="100%" height="500px" src="https://replit.com/@ChiefMigz/Pong?embed=true&lite=false#main.py"/>
+                    </div> 
+            </div>
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div className="pb-8">
                     <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Work</p>
                     <p className='py-4'>Check out some of my recent works</p>
                 </div>
-            
 
                 {/* Grid Item */}
                 <div className='grid sm:grid-cols-2 py-[-40px] md:grid-cols-3 gap-4 bg-[#0a192f]'>
                     {/* Item 1 */}
                     <div 
-                        style={{backgroundImage: `require(${WorkImg})`}}
+                        style={{backgroundImage: `url(${WebDesignImg})`}}
                         className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                         
                         {/* Hover Effects */}
@@ -23,37 +36,32 @@ const Work = () => {
                                 Website Design
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href="/">
+                                <a href="https://www.figma.com/proto/erB0se5rsmsF70gkHk9S1Z/Final-Design---Toronto-Cupcakes?page-id=0%3A1&node-id=183%3A506&viewport=223%2C318%2C0.16&scaling=scale-down-width&starting-point-node-id=183%3A506" target='_blank'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 text-lg'>
-                                        Demo
-                                    </button>
-                                </a>
-                                <a href="/">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 text-lg'>
-                                        Code
+                                        Link
                                     </button>
                                 </a>
                             </div>
                         </div>
                     </div>
 
-                    {/* Item 1 */}
+                    {/* Item 2 */}
                     <div 
-                        style={{backgroundImage: `require(${WorkImg})`}}
+                        style={{backgroundImage: `url(${PongGameImg})`}}
                         className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                         
                         {/* Hover Effects */}
                         <div className='opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
-                                Website Design
+                                Python Pong Game
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href="/">
+                                <a onClick={() => handleClick()}>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 text-lg'>
                                         Demo
                                     </button>
                                 </a>
-                                <a href="/">
+                                <a href="https://github.com/ChiefMigz/Pong" target='_blank'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 text-lg'>
                                         Code
                                     </button>
@@ -62,23 +70,18 @@ const Work = () => {
                         </div>
                     </div>
 
-                    {/* Item 1 */}
+                    {/* Item 3 */}
                     <div 
-                        style={{backgroundImage: `require(${WorkImg})`}}
+                        style={{backgroundImage: `url(${ThisWebsiteImg})`}}
                         className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                         
                         {/* Hover Effects */}
                         <div className='opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
-                                Website Design
+                                This website!
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href="/">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 text-lg'>
-                                        Demo
-                                    </button>
-                                </a>
-                                <a href="/">
+                                <a href="https://github.com/ChiefMigz/miguel-portfolio-website" target='_blank'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 text-lg'>
                                         Code
                                     </button>
@@ -86,7 +89,6 @@ const Work = () => {
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
